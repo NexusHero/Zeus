aaa774b170b6ef10deb464bad9c37f0b5c447ecf
NexusHero <suhay.sevinc@gmail.com>
2023-10-21 21:23:43 +0200

    First commit - Set up repository

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Zeus
+
+
+Zeus is a bot designed for the futures market, built on the ATAS platform. To compile this project, you'll require the ATAS libraries and the ATAS platform itself. Please note that we do not provide any guarantees for the performance of our bot, and this is not intended as investment advice.
+
+ATAS:
+https://atas.net/de/
---
+++ zeus/Zeus.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ATAS.Indicators.Technical
+{
+    using ATAS.Indicators;
+    using System.ComponentModel;
+    using Utils.Common.Logging;
+
+    [DisplayName("Zeus")]
+    public class Zeus : Indicator
+    {
+        //this method will be executed in realtime for every single candle 
+        protected override void OnCalculate(int bar, decimal value)
+        {
+            this.LogInfo("Start Calculating");
+        }
+
+        protected override void OnInitialize()
+        {
+            this.LogInfo("OnInitialize");
+
+            //Global property CurrentBar
+            var candle = GetCandle(CurrentBar - 1);
+            if (candle.Close > candle.Open)
+            {
+                this.LogInfo($"Last bar is bullish");
+            }
+            else
+            {
+                this.LogInfo($"Last bar is bearish");
+            }
+        }
+    }
+}
